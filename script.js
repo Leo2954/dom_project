20750fa7ff42c7b0db3e01202b3e761e331b30d9
german salcedo <leospillane2020@outlook.com>
2023-07-31 19:40:38 -0500

    second commit

--- script.js
+++ script.js
@@ -13,7 +13,19 @@ subtitle.innerHTML = "Here are Pokemon's incredible Gen 1 starters!";
 subtitle.style.textAlign = "center";
 
 //Changing starter image border to dots and changing color to yellow
+let imgDiv = document.getElementById("starters_image_div");
+imgDiv.style.border = "none";
+let startersImage = document.querySelector("#starters_image_div img");
+startersImage.style.border = "dotted";
+startersImage.style.borderColor = "yellow";
 
+//Adding yellow dotted border to pokemon images
+let images = document.getElementsByClassName("pokemon_images");
+for(let i = 0; i<images.length; i++)
+{
+    images[i].style.border = "dotted";
+    images[i].style.borderColor = "yellow";
+}
 
 //Changing Pokemon names to yellow and adding exclamation point
 let names = document.getElementsByClassName("pokemon_names");
@@ -22,3 +34,11 @@ for(let i = 0; i<names.length; i++)
     names[i].style.color = "yellow";
     names[i].innerHTML = names[i].innerHTML + "!";
 }
+
+//Changing footer to "All Star Code - DOM Project" and underlining and overlining and italicizing
+let footer = document.querySelector("footer");
+footer.innerHTML = "All Star Code - DOM Project";
+footer.style.fontSize = "30px";
+footer.style.textDecoration = "underline overline";
+footer.style.fontStyle = "italic";
+
